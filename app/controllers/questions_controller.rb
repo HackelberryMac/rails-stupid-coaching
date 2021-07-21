00621783839b75bb34862910a1d0b4d3c70463a8
HackelberryMac <danielcorigle@gmail.com>
2021-07-20 20:51:35 -0400

    Stupid Coaching

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,10 +1,16 @@
 class QuestionsController < ApplicationController
-
   def ask
   end
 
   def answer
+    question = params[:question]
+    @answer = ''
+    if question == 'estudiando'
+      @answer = 'Genial!'
+    elsif question.ends_with?('flojeando')
+      @answer = 'No seas Mula y ponte a estudiar!'
+    else
+      @answer = 'Sos entero MULA!!'
+    end
   end
-
-
 end
